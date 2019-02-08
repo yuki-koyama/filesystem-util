cbccbb8414c4ee83e938dfcb2a359ee17cb36e88
Yuki Koyama <yuki@koyama.xyz>
2019-02-08 14:26:13 +0900

    Add main files

---
+++ src/filesystem-util.cpp
@@ -0,0 +1,53 @@
+/*
+ MIT License
+ 
+ Copyright (c) 2019 Yuki Koyama
+ 
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ 
+ The above copyright notice and this permission notice shall be included in all
+ copies or substantial portions of the Software.
+ 
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ SOFTWARE.
+ */
+
+#include <filesystem-util.hpp>
+#include <sys/types.h>
+#include <dirent.h>
+#include <iostream>
+
+namespace filesystemutil
+{
+    std::vector<std::string> GetFileList(const std::string& dir_path, const std::string& suffix)
+    {
+        DIR* dir_ptr = opendir(dir_path.c_str());
+        if (dir_ptr == NULL)
+        {
+            std::cerr << "Could not open the directory." << std::endl;
+            return {};
+        }
+        struct dirent* dir_entry;
+        std::vector<std::string> file_list;
+        while ((dir_entry = readdir(dir_ptr)) != NULL)
+        {
+            const std::string entry_name = dir_entry->d_name;
+            if (entry_name.size() >= suffix.size() && entry_name.find(suffix, entry_name.size() - suffix.size()) != std::string::npos)
+            {
+                file_list.push_back(entry_name);
+            }
+        }
+        closedir(dir_ptr);
+        return file_list;
+    }
+}
